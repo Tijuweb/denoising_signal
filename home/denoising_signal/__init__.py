f3a1d1b06e08e08f1c9c9a186af97b1a20b7cecd
Tijuweb <julien.weber@hotmail.com>
2020-03-03 00:06:56 +0100

    Revert "Init packages and audio files"
    This reverts commit 5eaa5ab860b5a49edf481c5a14f2e9837fbe7c37.

--- home/denoising_signal/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-from home.denoising_signal.config import Config
-
-logging.basicConfig(format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
-logger = logging.getLogger(__name__)
-logger.setLevel(Config.LOGGING_LEVEL)
--- home/denoising_signal/config.py
+++
@@ -1,6 +0,0 @@
-import logging
-import os
-
-
-class Config:
-    LOGGING_LEVEL = os.environ.get("DENOISING_SIGNAL_LOGGER_LEVEL", logging.INFO)
